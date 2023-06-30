d2e8241d872ab9ec40d0eb4137fd0a5a600c7fa9
nn-firc <2319221757@qq.com>
2023-06-30 17:14:03 +0800

    shit

--- src_main/public/materialsystem/imesh.h
+++ src_main/public/materialsystem/imesh.h
@@ -1268,7 +1268,7 @@ inline void CVertexBuilder::FastVertex( const ModelVertexDX8_t &vertex )
 
 			emms
 	}
-#elif defined(GNUC)
+#elif defined(GNUC) && !defined(__arm__) || !defined(__aarch64__)
 	const void *pRead = &vertex;
 	void *pCurrPos = m_pCurrPosition;
 	__asm__ __volatile__ (
@@ -1324,7 +1324,7 @@ inline void CVertexBuilder::FastVertexSSE( const ModelVertexDX8_t &vertex )
 		movntps [edi + 16], xmm1
 		movntps [edi + 32], xmm2
 	}
-#elif defined(GNUC)
+#elif defined(GNUC) && !defined(__arm__) || !defined(__aarch64__)
 	const void *pRead = &vertex;
 	void *pCurrPos = m_pCurrPosition;
 	__asm__ __volatile__ (
